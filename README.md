fdca7620a9249be1c73e9dec01de10935c625e1b
Gregory Vincic <g@7de.se>
2022-02-22 22:29:08 +0100

    Add steps and initial pipeline

--- README.md
+++ README.md
@@ -2,3 +2,21 @@ tiny - The Go + github get started project
 
 Goals is to have a complete setup with pipelines in place for starting
 my next project.
+
+## Quick start
+
+    $ go install github.com/gregoryv/tiny/cmd/sayhi@latest
+	
+## Contribute
+
+Cross platform steps are defined in steps.go which pipelines use
+
+Build 
+
+    $ go run -tags steps steps.go build
+	
+Test
+
+    $ go run -tags steps steps.go test
+
+
---
+++ steps.go
@@ -0,0 +1,36 @@
+//go:build steps
+// +build steps
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+func main() {
+	for _, target := range os.Args[1:] {
+		switch target {
+		case "build":
+			sh("go", "build", filepath.Join("cmd", "sayhi"))
+
+		case "test":
+			sh("go", "test", "...")
+
+		default:
+			fmt.Fprint(os.Stderr, "unknown target:", target)
+			os.Exit(1)
+		}
+	}
+}
+
+func sh(app string, args ...string) {
+	c := exec.Command(app, args...)
+	c.Stdout = os.Stdout
+	c.Stderr = os.Stderr
+	if err := c.Run(); err != nil {
+		os.Exit(1)
+	}
+}
